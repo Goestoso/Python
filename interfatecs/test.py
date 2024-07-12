4f685d773d82fbcc8fec11fedaacf51431d6be06
Goes <gabriel.goes2107@gmail.com>
2024-07-12 18:24:41 -0300

    Object Oriented Programming Only

--- interfatecs/test.py
+++
@@ -1,6 +0,0 @@
-def imprimir_numeros(n):
-    if n > 0:
-        imprimir_numeros(n - 1)  # Chamada recursiva para imprimir os números de 1 a n-1
-        print(n)  # Imprime o número atual
-
-imprimir_numeros(10)  # Chama a função para imprimir os números de 1 a 10
